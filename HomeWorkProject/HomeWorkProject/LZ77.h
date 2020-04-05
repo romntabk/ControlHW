4b5f32d7bf2d801c6f3d81b6e072a04306f68286
ARTEMOV_ILYA <artemov.ilya.r@gmail.com>
2020-04-06 00:49:29 +0300

    maybe

--- HomeWorkProject/HomeWorkProject/LZ77.h
+++ HomeWorkProject/HomeWorkProject/LZ77.h
@@ -1,6 +1,7 @@
 #pragma once
 #include<vector>
 #include<iostream>
+#include<string>
 
 
 using namespace std;
@@ -39,53 +40,7 @@ private:
 
 	const int MAX_BUFF_SIZE = 5;
 
-	Node FindMatche(int pos, int buff_pos)
-	{
-		// 1 find all matches
-		vector<int> matches = vector<int>();
-		int first_char = buff_size + buff_pos;
-
-		if (buff_size == 0)
-		{
-			buff_size++;
-			return Node(0, 0, data[0]);
-		}
-
-		for (int i = buff_pos; i < buff_pos + buff_size; i++)
-		{
-			if (data[i] == data[first_char])
-				matches.push_back(i);
-		}
-
-		Node match;
-		Node max;
-
-		for (int c = 0; c < matches.size(); c++)
-		{
-			int i = matches[c];
-			int step = 0;
-			//     IN BUFF						 IN DATA
-			while (data[i - step * buff_size] == data[i + buff_size])
-			{
-				match.length += 1;
-				if (i - step * buff_size > buff_size + i)
-					step += 1;
-				i++;
-			}
-			match.offset = buff_size - matches[c];
-
-			match.next = data[buff_pos + buff_size + match.length];
-			
-			if (max.length < match.length)
-				max = match;
-		}
-		
-		if (buff_size < MAX_BUFF_SIZE)
-			buff_size++;
-		if (max.length == 0)
-			return Node(0, 0, data[pos + buff_size - 1]);
-		return max;
-	}
+	
 
 	int pos = 0;
 public:
@@ -102,13 +57,75 @@ public:
 		{
 			Node match = FindMatche(pos, buff_pos);
 			if (buff_size >= MAX_BUFF_SIZE)
-				buff_pos += match.length + 1;
+			{
+				buff_pos += match.length;
+				if (match.length == 0)
+					buff_pos++;
+			}
 			pos += match.length;
 			cout << match.ToString() << endl;
 			encode.push_back(match);
 		}		
 	}
 
+
+	Node FindMatche(int pos, int buff_pos)
+	{
+		// 1 find all matches
+		vector<int> matches = vector<int>();
+
+		//if (buff_pos != 0)
+		//	buff_pos -= 1;
+
+		int first_char = buff_size + buff_pos;
+
+		if (buff_size == 0)
+		{
+			buff_size++;
+			return Node(0, 0, data[0]);
+		}
+
+		for (int i = buff_pos; i < buff_pos + buff_size; i++)
+		{
+			if (data[i] == data[first_char])
+				matches.push_back(i);
+		}
+
+		Node max;
+
+		for (int c = 0; c < matches.size(); c++)
+		{
+			Node match;
+
+			int i = matches[c];
+			int step = 0;
+			//     IN BUFF						 IN DATA
+			while (data[i - step * buff_size] == data[i + buff_size - (matches[c] - buff_pos)])
+			{
+				match.length += 1;
+				if (i - step * buff_size > buff_size + i)
+					step += 1;
+				i++;
+			}
+			match.offset = buff_size - (matches[c] - buff_pos);
+
+			match.next = data[buff_pos + buff_size + match.length];
+
+			if (max.length < match.length)
+				max = match;
+		}
+
+		if (buff_size < MAX_BUFF_SIZE)
+		{
+			buff_size += max.length + 1;
+			if (buff_size > MAX_BUFF_SIZE)
+				buff_size = MAX_BUFF_SIZE;
+		}
+		if (max.length == 0)
+			return Node(0, 0, data[pos + buff_size - 1]);
+		return max;
+	}
+	
 	vector<Node>  GetEncode()
 	{
 		return encode;
