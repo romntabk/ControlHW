f80a01cedda08922810d2b185ef24d16900fa4d7
ARTEMOV_ILYA <artemov.ilya.r@gmail.com>
2020-04-05 22:34:08 +0300

    second try)

--- HomeWorkProject/HomeWorkProject/LZ77.h
+++ HomeWorkProject/HomeWorkProject/LZ77.h
@@ -11,6 +11,20 @@ public:
 	int offset = 0;
 	int length = 0;
 	char next = 0;
+
+	Node(int a, int b, char c)
+	{
+		offset = a;
+		length = b;
+		next = c;
+	}
+
+	Node() {}
+
+	string ToString()
+	{
+		return "(" + to_string(offset) + ", " + to_string(length) + ", " + next + ")";
+	}
 };
 
 class LZ77
@@ -21,7 +35,57 @@ private:
 	vector<Node> encode = vector<Node>();
 
 	int buff_pos = 0;
-	int buff_size = 10;
+	int buff_size = 0;
+
+	const int MAX_BUFF_SIZE = 5;
+
+	Node FindMatche(int pos, int buff_pos)
+	{
+		// 1 find all matches
+		vector<int> matches = vector<int>();
+		int first_char = buff_size + buff_pos;
+
+		if (buff_size == 0)
+		{
+			buff_size++;
+			return Node(0, 0, data[0]);
+		}
+
+		for (int i = buff_pos; i < buff_pos + buff_size; i++)
+		{
+			if (data[i] == data[first_char])
+				matches.push_back(i);
+		}
+
+		Node match;
+		Node max;
+
+		for (int c = 0; c < matches.size(); c++)
+		{
+			int i = matches[c];
+			int step = 0;
+			//     IN BUFF						 IN DATA
+			while (data[i - step * buff_size] == data[i + buff_size])
+			{
+				match.length += 1;
+				if (i - step * buff_size > buff_size + i)
+					step += 1;
+				i++;
+			}
+			match.offset = buff_size - matches[c];
+
+			match.next = data[buff_pos + buff_size + match.length];
+			
+			if (max.length < match.length)
+				max = match;
+		}
+		
+		if (buff_size < MAX_BUFF_SIZE)
+			buff_size++;
+		if (max.length == 0)
+			return Node(0, 0, data[pos + buff_size - 1]);
+		return max;
+	}
 
 	int pos = 0;
 public:
@@ -32,64 +96,28 @@ public:
 		this->size = size;
 	}
 
-	bool Endode()
+	void Encode()
 	{
-		while (pos < size)
+		while (buff_pos + buff_size < size)
 		{
 			Node match = FindMatche(pos, buff_pos);
-			buff_pos += match.length + 1;
+			if (buff_size >= MAX_BUFF_SIZE)
+				buff_pos += match.length + 1;
 			pos += match.length;
-			match.next = data[pos];
+			cout << match.ToString() << endl;
 			encode.push_back(match);
-		}
+		}		
 	}
 
-	Node FindMatche(int pos, int buff_pos)
+	vector<Node>  GetEncode()
 	{
-		// 1 find all matches
-		vector<int> matches = vector<int>();
-		int first_char = pos + 1;
-		for (int i = buff_pos; i < buff_pos + buff_size; i++)
-		{
-			if (data[i] == data[first_char])
-				matches.push_back(i);
-		}
-
-		vector<Node> node_maches = vector<Node>();
-
-		Node match;
-		Node max;
-		for (int c = 0; c < matches.size(); c++)
-		{
-			int i = buff_pos;
-			int step = 0;
-			//     IN BUFF    IN DATA
-			while (data[i - step * buff_size] == data[i + buff_size])
-			{
-				match.length += 1;
-				if (i - step * buff_size > buff_size + i)
-					step += 1;
-			}
-			match.offset = buff_size - matches[c];
-			match.next = data[first_char];
-			if (max.length < match.length)
-				max = match;
-		}
+		return encode;
 	}
+
+	int GetEncodeSize()
+	{
+		return encode.size();
+	}
+
 
 };
-
-/*
-//  — исходная строка
-// функция возвращает список блоков
-list<Node> encodeLZ77(string s):
-   list<Node> ans = []
-   pos = 0
-   while pos < s.length:
-	   offset, length = findMatching(buffer, pos)   // ищем слово в словаре
-	   shiftBuffer(length + 1)                      // перемещаем скользящее окно
-	   pos += length
-	   ans.push({offset, length, s[pos]})           // добавляем в ответ очередной блок
-   return ans
-
-*/
